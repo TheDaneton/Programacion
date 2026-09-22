607177012d4203a98f14730655ba5d7114f59229
DaniMarquez <danielinmr07@gmail.com>
2026-09-22 14:16:47 +0200

    Update EJ4.java

--- tema1/EJ4.java
+++ tema1/EJ4.java
@@ -1,15 +1,15 @@
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("English\tEspañol");
-		System.out.println("House\tCasa");
+		System.out.println("Sun\tSol");
+		System.out.println("Cat\tGato");
+		System.out.println("Bedroom\tCuarto");
 		System.out.println("Car\tCoche");
 		System.out.println("Dog\tPerro");
-		System.out.println("Cat\tGato");
-		System.out.println("Book\tLibro");
+		System.out.println("Videogame\tVideojuego");
 		System.out.println("Table\tMesa");
-		System.out.println("Chair\tSilla");
+		System.out.println("English\tEspañol");
+		System.out.println("Cock\tPollo");
 		System.out.println("Water\tAgua");
-		System.out.println("Sun\tSol");
-		System.out.println("Moon\tLuna");
+		System.out.println("Apple\tManzana");
 	}
 }
