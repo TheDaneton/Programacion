690ef89609f265534e2cadb6c9581d39cc09b260
DaniMarquez <danielinmr07@gmail.com>
2026-09-22 13:01:35 +0200

    Update EJ3.java

--- tema1/EJ3.java
+++ tema1/EJ3.java
@@ -1,7 +1,7 @@
 public class Main {
-public static void main(String[] args) {
-System.out.println("Daniel Márquez Riveiros");
-System.out.println("Calle Colón, 63");
-System.out.println("698 51 50 98");
-}
+  public static void main(String[] args) {
+    System.out.println("Daniel Márquez Riveiros");
+    System.out.println("Calle Colón, 63");
+    System.out.println("698 51 50 98");
+  }
 }
