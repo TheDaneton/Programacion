dadf58fc46c5c32a1f07b3b8bdfdc50c9597883b
DaniMarquez <danielinmr07@gmail.com>
2026-09-22 13:24:21 +0200

    Add files via upload

---
+++ tema1/EJ5.java
@@ -0,0 +1,61 @@
+public class Horario {
+
+    public static void main(String[] args) {
+
+        System.out.println("HORARIO 1º A DAW");
+        System.out.println();
+
+        System.out.printf("%-13s %-30s %-30s %-30s %-30s %-30s%n",
+                "Hora", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes");
+
+        System.out.println("---------------------------------------------------------------------------------------------------------------");
+
+        System.out.printf("%-13s %-30s %-30s %-30s %-30s %-30s%n",
+                "8:00-9:00",
+                "Lenguajes de Marcas",
+                "Bases de Datos",
+                "Sistemas Informáticos",
+                "Programación",
+                "Entorno de Desarrollo");
+
+        System.out.printf("%-13s %-30s %-30s %-30s %-30s %-30s%n",
+                "9:00-10:00",
+                "Lenguajes de Marcas",
+                "Bases de Datos",
+                "Sistemas Informáticos",
+                "Programación",
+                "Bases de Datos");
+
+        System.out.printf("%-13s %-30s %-30s %-30s %-30s %-30s%n",
+                "10:00-11:00",
+                "Sistemas Informáticos",
+                "Sostenibilidad",
+                "Itinerario Personal para la Empleabilidad I",
+                "Lenguajes de Marcas",
+                "Bases de Datos");
+
+        System.out.printf("%-13s %-30s %-30s %-30s %-30s %-30s%n",
+                "11:30-12:30",
+                "Sistemas Informáticos",
+                "Entorno de Desarrollo",
+                "Itinerario Personal para la Empleabilidad I",
+                "Sistemas Informáticos",
+                "Itinerario Personal para la Empleabilidad I");
+
+        System.out.printf("%-13s %-30s %-30s %-30s %-30s %-30s%n",
+                "12:30-13:30",
+                "Programación",
+                "Programación",
+                "Digitalización",
+                "Bases de Datos",
+                "Programación");
+
+        System.out.printf("%-13s %-30s %-30s %-30s %-30s %-30s%n",
+                "13:30-14:30",
+                "Programación",
+                "Programación",
+                "Entorno de Desarrollo",
+                "Bases de Datos",
+                "Programación");
+    }
+}
